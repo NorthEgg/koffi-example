fa4cad35777e6a2b41d33b70ff63f08d5875c38e
NorthEgg <996581176@qq.com>
2025-09-25 16:45:51 +0800

    fix: 修复GetNetworkParams函数使用时未正确分配缓冲区的bug

--- src/GetNetworkParams.ts
+++ src/GetNetworkParams.ts
@@ -31,7 +31,10 @@ const aa = load<{
   /**检索本地计算机的网络参数
    * @see https://learn.microsoft.com/zh-cn/windows/win32/api/iphlpapi/nf-iphlpapi-getnetworkparams
    */
-  GetNetworkParams: (pFixedInfo: Buffer<ArrayBuffer>, pOutBufLen: number[]) => number; // 定义要使用的函数类型
+  GetNetworkParams: (
+    pFixedInfo: Buffer<ArrayBuffer> | null,
+    pOutBufLen: Buffer<ArrayBuffer>
+  ) => number; // 定义要使用的函数类型
 }>({
   dll: "iphlpapi.dll", // 使用的dll库
   // 定义函数返回值以及入参类型
@@ -45,17 +48,25 @@ const aa = load<{
   // 是否自动创建结构体
   autoCreateStruct: false,
 });
-
-const bufferSize = koffi.sizeof("FIXED_INFO"); // 获取 FIXED_INFO 结构体的大小
-const pFixedInfo = Buffer.alloc(bufferSize); // 分配一个指定大小的缓冲区
-
 // koffi 加载方式
 const qq = iphlpapi.func(CallingConvention.Stdcall, "GetNetworkParams", "DWORD", [
   koffi.out(koffi.pointer("FIXED_INFO")), // 参数为 FIXED_INFO 结构体指针。 out 类型需要使用这种方式创建 更多资料参考 https://koffi.dev/output
   "ulong *", // 基础类型指针
 ]);
 
-// pFixedInfo 也可以使用一个空对象代替
+// 先查询所需缓冲区大小，再分配内存
+const pBufferSize = Buffer.alloc(8);
+// pFixedInfo 也可以使用一个空对象或null代替
 // 传递基础类型指针使用 [] 包裹，如下面的 [bufferSize]
-const q = aa.GetNetworkParams(pFixedInfo, [bufferSize]); // win32-def 加载方式调用
-// const q = qq(pFixedInfo, [bufferSize]); // koffi 加载方式调用
+aa.GetNetworkParams(null, pBufferSize); // win32-def 加载方式调用
+// 上面这步获取缓冲区大小存在 pBufferSize 中
+
+// 获取 FIXED_INFO 结构体的大小，两种方式
+const fixed_info_size = koffi.sizeof("FIXED_INFO"); // 使用 koffi.struct() 创建结构体的名称
+// const fixed_info_size = koffi.sizeof(FIXED_INFO); // 使用 koffi.struct() 创建结构体的返回值
+
+const pFixedInfo = Buffer.alloc(fixed_info_size); // 根据前面获取的缓冲区大小，分配缓冲区
+qq(pFixedInfo, pBufferSize); // koffi 加载方式调用
+
+// 解析数据
+console.log(koffi.decode(pFixedInfo, FIXED_INFO));
